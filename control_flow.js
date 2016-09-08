8a81a8495a5d792b827967dc1b23fd4b6225d4af
rhamill1 <rhamill11@gmail.com>
2016-09-07 21:15:39 -0700

    control flow hw night2

---
+++ control_flow.js
@@ -0,0 +1,88 @@
+
+//take 1
+var tokens = 3;
+var age = 7;
+var height = 4.5;
+
+if (tokens >= 5) {
+    if(age >= 5) {
+        if(height >= 4) {
+    console.log("Step right up!");}
+    ;};}
+else {console.log("hit the road you deadbeat, short preschooler!");}
+
+
+
+
+//take 2
+var tokens = 3;
+var age = 7;
+var height = 4.5;
+var wadult = True;
+
+if (tokens >= 5 && age >= 12 && height >= 4){
+    console.log("Step right up!");}
+
+else if (tokens >= 5 && age >= 5 && height >= 4 && wadult == True) {
+    console.log("Step right up!");}
+
+else {console.log("hit the road you deadbeat, short, preschooler!");}
+
+
+
+
+//take 3
+var tokens = 3;
+var age = 7;
+var height = 4.5;
+var wadult = True;
+var bossLooking = False;
+
+if (bossLooking == Fales) {
+    console.log("ahoy hoy!  bienvenidos!");}
+
+else if (tokens >= 5 && age >= 12 && height >= 4){
+    console.log("Step right up!");}
+
+else if (tokens >= 5 && age >= 5 && height >= 4 && wadult == True) {
+    console.log("Step right up!");}
+
+else {console.log("hit the road you deadbeat, short, preschooler!");}
+
+
+
+
+//take 4
+var tokens = 3;
+var age = 7;
+var height = 4.5;
+var wadult = True;
+var bossLooking = False;
+var parkPass = False;
+
+if (bossLooking == False || parkPass== True) {
+    console.log("ahoy hoy!  bienvenidos!");}
+
+else if (tokens >= 5 && age >= 12 && height >= 4){
+    console.log("Step right up!");}
+
+else if (tokens >= 5 && age >= 5 && height >= 4 && wadult == True) {
+    console.log("Step right up!");}
+
+else {console.log("hit the road you deadbeat, short, preschooler!");}
+
+
+
+
+
+
+// switch
+switch(joke.isFunny) {
+    case 'yes':
+        console.log(laugh());
+        break;
+    case 'sort of':
+        console.log(chortle());
+        break;
+    default:
+        console.log(stareBlankly());}
